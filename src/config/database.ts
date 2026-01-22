243eb712cef67262ecd3caedbf9308c25899db14
PaduanFrancisco <video.amigos2@gmail.com>
2026-01-21 22:06:54 -0300

    Configuracion

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    const mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017/carpinteria';
+    const mongoURI = process.env.MONGO_URI || 'mongodb+srv://Vercel-Admin-atlas-purple-island:pDAQxcCsVE8VIUB7@atlas-purple-island.bbdxhnz.mongodb.net/?retryWrites=true&w=majority';
     await mongoose.connect(mongoURI);
     console.log('✅ MongoDB conectado correctamente');
   } catch (error) {
